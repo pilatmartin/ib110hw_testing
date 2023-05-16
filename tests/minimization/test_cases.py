1cd4a0d9e010b4446027bdc38554081d8f690af7
Martin Pilát <8pilatmartin8@gmail.com>
2023-05-16 23:06:59 +0200

    refac: updated test cases to be more clear

--- tests/minimization/test_cases.py
+++ tests/minimization/test_cases.py
@@ -1,190 +1,261 @@
-from ib110hw.automaton.dfa import DFA, DFATransitions
+from ib110hw.automaton.dfa import DFA
 
-ex1_transitions: DFATransitions = {
-    "0": {"a": "1", "b": "2"},
-    "1": {"a": "1", "b": "3"},
-    "2": {"a": "1", "b": "2"},
-    "3": {"a": "1", "b": "4"},
-    "4": {"a": "1", "b": "2"},
-}
 
-ex1_transitions_expected: DFATransitions = {
-    "02": {"a": "1", "b": "02"},
-    "1": {"a": "1", "b": "3"},
-    "3": {"a": "1", "b": "4"},
-    "4": {"a": "1", "b": "02"},
-}
-
-ex1_automaton: DFA = DFA(
-    {"0", "1", "2", "3", "4"}, {"a", "b"}, "0", {"4"}, ex1_transitions
+one_nondistinguishable: DFA = DFA(
+    {"0", "1", "2", "3", "4"},
+    {"a", "b"},
+    "0",
+    {"4"},
+    {
+        "0": {"a": "1", "b": "2"},
+        "1": {"a": "1", "b": "3"},
+        "2": {"a": "1", "b": "2"},
+        "3": {"a": "1", "b": "4"},
+        "4": {"a": "1", "b": "2"},
+    },
 )
 
-ex1_automaton_expected: DFA = DFA(
-    {"02", "1", "3", "4"}, {"a", "b"}, "02", {"4"}, ex1_transitions_expected
+one_nondistinguishable_expected: DFA = DFA(
+    {"02", "1", "3", "4"},
+    {"a", "b"},
+    "02",
+    {"4"},
+    {
+        "02": {"a": "1", "b": "02"},
+        "1": {"a": "1", "b": "3"},
+        "3": {"a": "1", "b": "4"},
+        "4": {"a": "1", "b": "02"},
+    },
 )
 
-ex2_transitions: DFATransitions = {
-    "0": {"a": "1", "b": "0"},
-    "1": {"a": "2", "b": "1"},
-    "2": {"a": "1", "b": "2"},
-    "3": {"a": "1", "b": "2"},
-}
-
-ex2_transitions_expected: DFATransitions = {
-    "0": {"a": "12", "b": "0"},
-    "12": {"a": "12", "b": "12"},
-}
-
-ex2_automaton: DFA = DFA(
-    {"0", "1", "2", "3"}, {"a", "b"}, "0", {"1", "2"}, ex2_transitions
+two_nondistinguishable: DFA = DFA(
+    {"0", "1", "2", "3"},
+    {"a", "b"},
+    "0",
+    {"1", "2"},
+    {
+        "0": {"a": "1", "b": "0"},
+        "1": {"a": "2", "b": "1"},
+        "2": {"a": "1", "b": "2"},
+        "3": {"a": "1", "b": "2"},
+    },
 )
 
-ex2_automaton_expected: DFA = DFA(
-    {"0", "12"}, {"a", "b"}, "0", {"12"}, ex2_transitions_expected
+two_nondistinguishable_expected: DFA = DFA(
+    {"0", "12"},
+    {"a", "b"},
+    "0",
+    {"12"},
+    {
+        "0": {"a": "12", "b": "0"},
+        "12": {"a": "12", "b": "12"},
+    },
 )
 
-ex3_transitions: DFATransitions = {
-    "0": {"a": "1", "b": "3"},
-    "1": {"a": "2", "b": "4"},
-    "2": {"a": "1", "b": "4"},
-    "3": {"a": "2", "b": "4"},
-    "4": {"a": "4", "b": "4"},
-}
-
-ex3_transitions_expected: DFATransitions = {
-    "0": {"a": "123", "b": "123"},
-    "123": {"a": "123", "b": "4"},
-    "4": {"a": "4", "b": "4"},
-}
-
-ex3_automaton: DFA = DFA(
-    {"0", "1", "2", "3", "4"}, {"a", "b"}, "0", {"4"}, ex3_transitions
+three_nondistinguishable: DFA = DFA(
+    {"0", "1", "2", "3", "4", "5"},
+    {"a", "b"},
+    "0",
+    {"3", "4"},
+    {
+        "0": {"a": "1", "b": "2"},
+        "1": {"a": "2", "b": "3"},
+        "2": {"a": "2", "b": "4"},
+        "3": {"a": "3", "b": "3"},
+        "4": {"a": "4", "b": "4"},
+        "5": {"a": "5", "b": "4"},
+    },
 )
 
-ex3_automaton_expected: DFA = DFA(
-    {"0", "123", "4"}, {"a", "b"}, "0", {"4"}, ex3_transitions_expected
+three_nondistinguishable_expected: DFA = DFA(
+    {"0", "125", "34"},
+    {"a", "b"},
+    "0",
+    {"34"},
+    {
+        "0": {"a": "125", "b": "125"},
+        "125": {"a": "125", "b": "34"},
+        "34": {"a": "34", "b": "34"},
+    },
 )
 
-ex4_transitions: DFATransitions = {
-    "0": {"a": "1", "b": "2"},
-    "1": {"a": "2", "b": "3"},
-    "2": {"a": "2", "b": "4"},
-    "3": {"a": "3", "b": "3"},
-    "4": {"a": "4", "b": "4"},
-    "5": {"a": "5", "b": "4"},
-}
-
-ex4_transition_expected: DFATransitions = {
-    "0": {"a": "125", "b": "125"},
-    "125": {"a": "125", "b": "34"},
-    "34": {"a": "34", "b": "34"},
-}
-
-ex4_automaton: DFA = DFA(
-    {"0", "1", "2", "3", "4", "5"}, {"a", "b"}, "0", {"3", "4"}, ex4_transitions
+random: DFA = DFA(
+    {"0", "1", "2", "3", "4"},
+    {"a", "b"},
+    "0",
+    {"4"},
+    {
+        "0": {"a": "1", "b": "3"},
+        "1": {"a": "2", "b": "4"},
+        "2": {"a": "1", "b": "4"},
+        "3": {"a": "2", "b": "4"},
+        "4": {"a": "4", "b": "4"},
+    },
 )
 
-ex4_automaton_expected: DFA = DFA(
-    {"0", "125", "34"}, {"a", "b"}, "0", {"34"}, ex4_transition_expected
+random_expected: DFA = DFA(
+    {"0", "123", "4"},
+    {"a", "b"},
+    "0",
+    {"4"},
+    {
+        "0": {"a": "123", "b": "123"},
+        "123": {"a": "123", "b": "4"},
+        "4": {"a": "4", "b": "4"},
+    },
 )
 
-ex5_transitions = {
-    "A": {"a": "B", "b": "C"},
-    "B": {"a": "D", "b": "E"},
-    "C": {"a": "C", "b": "C"},
-    "D": {"a": "B", "b": "E"},
-    "E": {"a": "F", "b": "E"},
-    "F": {"a": "G", "b": "E"},
-    "G": {"a": "D", "b": "E"},
-}
-
-ex5_transitions_expected: DFATransitions = {
-    "A": {"a": "BDFG", "b": "C"},
-    "C": {"a": "C", "b": "C"},
-    "BDFG": {"a": "BDFG", "b": "E"},
-    "E": {"a": "BDFG", "b": "E"},
-}
-
-ex5_automaton = DFA(
+random2 = DFA(
     {"A", "B", "C", "D", "E", "F", "G"},
     {"a", "b"},
     "A",
     {"B", "D", "F", "G"},
-    ex5_transitions,
+    {
+        "A": {"a": "B", "b": "C"},
+        "B": {"a": "D", "b": "E"},
+        "C": {"a": "C", "b": "C"},
+        "D": {"a": "B", "b": "E"},
+        "E": {"a": "F", "b": "E"},
+        "F": {"a": "G", "b": "E"},
+        "G": {"a": "D", "b": "E"},
+    },
 )
-ex5_automaton_expected: DFA = DFA(
+random2_expected: DFA = DFA(
     {"A", "C", "E", "BDFG"},
     {"a", "b"},
     "A",
     {"BDFG"},
-    ex5_transitions_expected,
+    {
+        "A": {"a": "BDFG", "b": "C"},
+        "C": {"a": "C", "b": "C"},
+        "BDFG": {"a": "BDFG", "b": "E"},
+        "E": {"a": "BDFG", "b": "E"},
+    },
 )
 
-minimal_transitions: DFATransitions = {
-    "1": {"a": "2", "b": "4"},
-    "2": {"a": "4", "b": "3"},
-    "3": {"a": "3", "b": "6"},
-    "4": {"a": "4", "b": "5"},
-    "5": {"a": "5", "b": "5"},
-    "6": {"a": "2", "b": "5"},
-}
 
-minimal_transitions_expected: DFATransitions = minimal_transitions
-
-minimal_automaton: DFA = DFA(
-    {"1", "2", "3", "4", "5", "6"}, {"a", "b"}, "1", {"3", "5"}, minimal_transitions
+minimal: DFA = DFA(
+    {"1", "2", "3", "4", "5", "6"},
+    {"a", "b"},
+    "1",
+    {"3", "5"},
+    {
+        "1": {"a": "2", "b": "4"},
+        "2": {"a": "4", "b": "3"},
+        "3": {"a": "3", "b": "6"},
+        "4": {"a": "4", "b": "5"},
+        "5": {"a": "5", "b": "5"},
+        "6": {"a": "2", "b": "5"},
+    },
 )
 
-minimal_automaton_expected: DFA = minimal_automaton
-
-minimal2_transitions = {
-    "A": {"a": "B", "b": "D"},
-    "B": {"a": "D", "b": "C"},
-    "C": {"a": "C", "b": "F"},
-    "D": {"a": "D", "b": "E"},
-    "E": {"a": "E", "b": "E"},
-    "F": {"a": "B", "b": "E"},
-}
-
-minimal2_transitions_expected: DFATransitions = minimal2_transitions
-
-minimal2_automaton: DFA = DFA(
-    {"A", "B", "C", "D", "E", "F"}, {"a", "b"}, "A", {"C", "E"}, minimal2_transitions
+minimal_expected: DFA = DFA(
+    {"1", "2", "3", "4", "5", "6"},
+    {"a", "b"},
+    "1",
+    {"3", "5"},
+    {
+        "1": {"a": "2", "b": "4"},
+        "2": {"a": "4", "b": "3"},
+        "3": {"a": "3", "b": "6"},
+        "4": {"a": "4", "b": "5"},
+        "5": {"a": "5", "b": "5"},
+        "6": {"a": "2", "b": "5"},
+    },
 )
 
-minimal2_automaton_expected = minimal2_automaton
-
-two_equivalent_states_transitions: DFATransitions = {
-    "1": {"a": "2", "b": "2"},
-    "2": {"a": "1", "b": "1"},
-}
-
-two_equivalent_states_transitions_expected = {"1": {"a": "1", "b": "1"}}
-
-two_equivalent_states_automaton: DFA = DFA(
-    {"1", "2"}, {"a", "b"}, "1", {"1", "2"}, two_equivalent_states_transitions
+minimal2: DFA = DFA(
+    {"A", "B", "C", "D", "E", "F"},
+    {"a", "b"},
+    "A",
+    {"C", "E"},
+    {
+        "A": {"a": "B", "b": "D"},
+        "B": {"a": "D", "b": "C"},
+        "C": {"a": "C", "b": "F"},
+        "D": {"a": "D", "b": "E"},
+        "E": {"a": "E", "b": "E"},
+        "F": {"a": "B", "b": "E"},
+    },
 )
 
-two_equivalent_states_automaton_expected = DFA(
-    {"1"}, {"a", "b"}, "1", {"1"}, two_equivalent_states_transitions_expected
+minimal2_expected = DFA(
+    {"A", "B", "C", "D", "E", "F"},
+    {"a", "b"},
+    "A",
+    {"C", "E"},
+    {
+        "A": {"a": "B", "b": "D"},
+        "B": {"a": "D", "b": "C"},
+        "C": {"a": "C", "b": "F"},
+        "D": {"a": "D", "b": "E"},
+        "E": {"a": "E", "b": "E"},
+        "F": {"a": "B", "b": "E"},
+    },
 )
 
-disjoint_transitions: DFATransitions = {
-    "1": {"a": "1", "b": "1"},
-    "2": {"a": "2", "b": "2"},
-    "3": {"a": "3", "b": "3"},
-    "4": {"a": "4", "b": "4"},
-    "5": {"a": "5", "b": "5"},
-}
-
-disjoint_transitions_expected: DFATransitions = {
-    "1": {"a": "1", "b": "1"},
-}
-
-disjoint_automaton: DFA = DFA(
-    {"1", "2", "3", "4", "5"}, {"a", "b"}, "1", set(), disjoint_transitions
+two_equivalent_states: DFA = DFA(
+    {"1", "2"},
+    {"a", "b"},
+    "1",
+    {"1", "2"},
+    {
+        "1": {"a": "2", "b": "2"},
+        "2": {"a": "1", "b": "1"},
+    },
 )
 
-disjoint_automaton_expected: DFA = DFA(
-    {"1"}, {"a", "b"}, "1", set(), disjoint_transitions_expected
+two_equivalent_states_expected = DFA(
+    {"1"}, {"a", "b"}, "1", {"1"}, {"1": {"a": "1", "b": "1"}}
+)
+
+disjoint: DFA = DFA(
+    {"1", "2", "3", "4", "5"},
+    {"a", "b"},
+    "1",
+    set(),
+    {
+        "1": {"a": "1", "b": "1"},
+        "2": {"a": "2", "b": "2"},
+        "3": {"a": "3", "b": "3"},
+        "4": {"a": "4", "b": "4"},
+        "5": {"a": "5", "b": "5"},
+    },
+)
+
+disjoint_expected: DFA = DFA(
+    {"1"},
+    {"a", "b"},
+    "1",
+    set(),
+    {
+        "1": {"a": "1", "b": "1"},
+    },
+)
+
+
+empty_language_with_final_states: DFA = DFA(
+    {"s1", "s2", "s3", "s4", "s5"},
+    {"a", "b"},
+    "s1",
+    {"s1", "s4", "s5"},
+    {
+        "s1": {"a": "s2", "b": "s3"},
+        "s2": {"a": "s3", "b": "s3"},
+        "s3": {"a": "s3", "b": "s3"},
+        "s4": {"a": "s5", "b": "s1"},
+        "s5": {"a": "s1", "b": "s4"},
+    },
+)
+
+
+empty_language_with_final_states_expected: DFA = DFA(
+    {"s1", "s2"},
+    {"a", "b"},
+    "s1",
+    {"s1"},
+    {
+        "s1": {"a": "s2", "b": "s2"},
+        "s2": {"a": "s2", "b": "s2"},
+    },
 )
--- tests/minimization/tests.py
+++ tests/minimization/tests.py
@@ -24,49 +24,54 @@ class TestMinimization(unittest.TestCase):
                 expected.transitions, actual.transitions, INVALID_TRANSITIONS
             )
 
-    def test1(self):
-        actual = canonize(minimize(ex1_automaton))
-        expected = canonize(ex1_automaton_expected)
+    def test_one_nondistinguishable(self):
+        actual = canonize(minimize(one_nondistinguishable))
+        expected = canonize(one_nondistinguishable_expected)
         self.are_valid(expected, actual)
 
-    def test2(self):
-        actual = canonize(minimize(ex2_automaton))
-        expected = canonize(ex2_automaton_expected)
+    def test_two_nondistinguishable(self):
+        actual = canonize(minimize(two_nondistinguishable))
+        expected = canonize(two_nondistinguishable_expected)
         self.are_valid(expected, actual)
 
-    def test3(self):
-        actual = canonize(minimize(ex3_automaton))
-        expected = canonize(ex3_automaton_expected)
+    def test_random(self):
+        actual = canonize(minimize(random))
+        expected = canonize(random_expected)
         self.are_valid(expected, actual)
 
-    def test4(self):
-        actual = canonize(minimize(ex4_automaton))
-        expected = canonize(ex4_automaton_expected)
+    def test_random2(self):
+        actual = canonize(minimize(random2))
+        expected = canonize(random2_expected)
         self.are_valid(expected, actual)
 
-    def test5(self):
-        actual = canonize(minimize(ex5_automaton))
-        expected = canonize(ex5_automaton_expected)
+    def test_three_nondistinguishable(self):
+        actual = canonize(minimize(three_nondistinguishable))
+        expected = canonize(three_nondistinguishable_expected)
         self.are_valid(expected, actual)
 
     def test_minimal1(self):
-        actual = canonize(minimize(minimal_automaton))
-        expected = canonize(minimal_automaton_expected)
+        actual = canonize(minimize(minimal))
+        expected = canonize(minimal_expected)
         self.are_valid(expected, actual)
 
     def test_minimal2(self):
-        actual = canonize(minimize(minimal2_automaton_expected))
-        expected = canonize(minimal2_automaton_expected)
+        actual = canonize(minimize(minimal2_expected))
+        expected = canonize(minimal2_expected)
         self.are_valid(expected, actual)
 
     def test_two_equivalent_states(self):
-        actual = canonize(minimize(two_equivalent_states_automaton))
-        expected = canonize(two_equivalent_states_automaton_expected)
+        actual = canonize(minimize(two_equivalent_states))
+        expected = canonize(two_equivalent_states_expected)
         self.are_valid(expected, actual)
 
     def test_disjoint(self):
-        actual = canonize(minimize(disjoint_automaton))
-        expected = canonize(disjoint_automaton_expected)
+        actual = canonize(minimize(disjoint))
+        expected = canonize(disjoint_expected)
+        self.are_valid(expected, actual)
+
+    def test_empty_language_with_final_states_transitions(self):
+        actual = canonize(minimize(empty_language_with_final_states))
+        expected = canonize(empty_language_with_final_states_expected)
         self.are_valid(expected, actual)
 
 
